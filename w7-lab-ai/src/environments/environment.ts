a70876d807da9d06685cc1c3c8b0cd85950f857b
Joshua Monahan <123738720+jmonahanATU@users.noreply.github.com>
2024-11-18 15:13:57 +0000

    add apiKey to environment.ts and update .gitignore

--- w7-lab-ai/src/environments/environment.ts
+++ w7-lab-ai/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyDYbCPNAHnnXiydGnecdvKIexYzb8ZsYYU'  // Your instructor will provide 
 };
 
 /*
